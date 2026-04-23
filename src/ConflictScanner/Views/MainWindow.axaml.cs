6f3d73324370e073ae448b4e7b2f43cfbf3e7484
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-23 19:49:14 -0400

    Add MainWindow class for the application

--- src/ConflictScanner/Views/MainWindow.axaml.cs
+++ src/ConflictScanner/Views/MainWindow.axaml.cs
@@ -1,1 +1,12 @@
+using Avalonia.Controls;
 
+namespace ConflictScanner.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
