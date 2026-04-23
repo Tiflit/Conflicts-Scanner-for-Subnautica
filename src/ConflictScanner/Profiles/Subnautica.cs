f34388063dc7f9cd461e913f4117c3b174f3756a
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-23 15:17:13 -0400

    Rename SubnauticaProfile class to Subnautica

--- src/ConflictScanner/Profiles/Subnautica.cs
+++ src/ConflictScanner/Profiles/Subnautica.cs
@@ -2,13 +2,12 @@ using System.IO;
 
 namespace ConflictScanner.Profiles
 {
-    public class SubnauticaProfile : GameProfile
+    public class Subnautica : GameProfile
     {
         public override string GameName => "Subnautica";
 
         public override bool MatchesGame(string gamePath)
         {
-            // Simple heuristic: Subnautica.exe exists
             string exe = Path.Combine(gamePath, "Subnautica.exe");
             return File.Exists(exe);
         }
