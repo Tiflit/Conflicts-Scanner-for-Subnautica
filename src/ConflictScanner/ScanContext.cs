b7cd01a9ba690b493f05e234aaa097cb743859a6
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-23 14:16:43 -0400

    Refactor warning lists to include severity levels

--- src/ConflictScanner/ScanContext.cs
+++ src/ConflictScanner/ScanContext.cs
@@ -6,14 +6,34 @@ namespace ConflictScanner
     {
         public string GamePath { get; }
 
-        public List<string> HarmonyWarnings { get; } = new();
-        public List<string> NautilusWarnings { get; } = new();
-        public List<string> QModWarnings { get; } = new();
-        public List<string> FileWarnings { get; } = new();
+        public List<(Severity Level, string Message)> HarmonyWarnings { get; } = new();
+        public List<(Severity Level, string Message)> NautilusWarnings { get; } = new();
+        public List<(Severity Level, string Message)> QModWarnings { get; } = new();
+        public List<(Severity Level, string Message)> FileWarnings { get; } = new();
 
         public ScanContext(string gamePath)
         {
             GamePath = gamePath;
+        }
+
+        public void AddFileWarning(Severity level, string message)
+        {
+            FileWarnings.Add((level, message));
+        }
+
+        public void AddQModWarning(Severity level, string message)
+        {
+            QModWarnings.Add((level, message));
+        }
+
+        public void AddHarmonyWarning(Severity level, string message)
+        {
+            HarmonyWarnings.Add((level, message));
+        }
+
+        public void AddNautilusWarning(Severity level, string message)
+        {
+            NautilusWarnings.Add((level, message));
         }
     }
 }
